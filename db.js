87e975ce5406bab237013364226a9dc3ff87139a
Diana <diana2005rus64@mail.ru>
2025-05-15 08:56:32 +0400

    конфиг подключения бд и env

--- db.js
+++ db.js
@@ -1,19 +1,18 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 require('dotenv').config();
 
-const db = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-db.connect((err) => {
-  if (err) {
-    console.log('❌ Ошибка подключения к базе данных');
-  } else {
-    console.log('✅ База данных подключена!');
-  }
-});
+pool.getConnection()
+  .then(() => console.log('✅ База данных подключена!'))
+  .catch(err => console.error('❌ Ошибка подключения к базе данных:', err.message));
 
-module.exports = db;
+module.exports = pool;
